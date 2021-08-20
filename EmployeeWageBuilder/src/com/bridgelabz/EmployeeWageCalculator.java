f4fdc2584b6394063acb07286d70674cf162b110
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-21 01:24:52 +0530

    UPDATE: Add Wage Details

--- EmployeeWageBuilder/src/com/bridgelabz/EmployeeWageCalculator.java
+++ EmployeeWageBuilder/src/com/bridgelabz/EmployeeWageCalculator.java
@@ -3,11 +3,15 @@ package com.bridgelabz;
 public class EmployeeWageCalculator {
 	public static void main(String[] args) {
 		int fullTime = 1;
-		// Computation
+		int wagePerHour = 20;
+		int empHours = 0;
+		int empWage = 0;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == fullTime)
-		System.out.println("Employee is Present");
+			empHours = 8;
 		else
-		System.out.println("Employee is Absent");
+			empHours = 0;
+		empWage = empHours * wagePerHour;
+		System.out.println("Emp Wage:" + empWage) ;
 	}
 }
