8710d8dc62a5364ab57dba925e8069f0ab1aade5
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-21 01:54:21 +0530

    UPDATE: Wage for a month

--- EmployeeWageBuilder/src/com/bridgelabz/EmployeeWageCalculator.java
+++ EmployeeWageBuilder/src/com/bridgelabz/EmployeeWageCalculator.java
@@ -4,10 +4,12 @@ public class EmployeeWageCalculator {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
 	
 	public static void main(String[] args) {
-		int empHours = 0;
-		int empWage = 0;
+		int empHours = 0, empWage = 0,totalEmpWage=0;
+		for (int day = 0; day < NUM_OF_WORKING_DAYS;day++)
+		{
 		int empCheck = (int)Math.floor(Math.random() * 10) % 3;
 		switch (empCheck) {
 		case IS_FULL_TIME:
@@ -20,6 +22,9 @@ public class EmployeeWageCalculator {
 		empHours = 0;
 		}
 		empWage = empHours * EMP_RATE_PER_HOUR;
+		totalEmpWage += empWage;
 		System.out.println("Emp Wage:" + empWage) ;
+		}
+		System.out.println("Total Emp Wagefor a Month:" + totalEmpWage);
 	}
 }
