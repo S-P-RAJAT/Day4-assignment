b275504ec484004d47011ca94a2e376b1d7e4338
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-20 22:00:52 +0530

    UPDATE: Add Single Player option

--- SnakeAndLadderGame/src/com/bridgelabz/SnakeAndLadder.java
+++ SnakeAndLadderGame/src/com/bridgelabz/SnakeAndLadder.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
 
+import java.util.Iterator;
+
 public class SnakeAndLadder {
+	int position;
+	
+	public SnakeAndLadder() {
+		this.position = 0;
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game!");
+		System.out.println("");
+		SnakeAndLadder player1 = new SnakeAndLadder();
 	}
 }
